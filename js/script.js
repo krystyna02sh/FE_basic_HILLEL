a6cb9048fa558c8ad33c235200f38f99ecd681b7
Krystyna Shunkina <christina.shunkina@gmail.com>
2023-10-11 22:33:45 +0300

    add burger to index file

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 console.log('It work!');
+
+$('.burger-container').click(function () {
+    $('.burger-container').toggleClass('active');
+    $('.burger').addClass('active');
+    $('.wrapper-nav').addClass('active');
+    $('.flex-nav').addClass('active');
+    const target = $(this).data('target');
+    $('target').addClass('active');
+});
+
+$('.wrapper-nav, .flex-nav .close').click(function (event) {
+    if (this === event.target) {
+        $('.wrapper-nav').removeClass('active');
+        $('.flex-nav').removeClass('active');
+        $('.burger').removeClass('active');
+    }
+
+});
